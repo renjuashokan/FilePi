a3f68d5182eb72510a411e034d2425f4c9e87186
Renju Ashokan <renjuashokan@gmail.com>
2025-02-27 00:07:12 +0530

    Formatting and updating pip

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-
-
 def run_server():
     """
     Entrypoint for the filepi command.
     """
-    from . import LOGGING_CONFIG, app
     import uvicorn
 
+    from . import LOGGING_CONFIG, app
+
     uvicorn.run(app, host="0.0.0.0", port=8080, log_config=LOGGING_CONFIG, log_level="info")
+
 
 if __name__ == "__main__":
     run_server()
